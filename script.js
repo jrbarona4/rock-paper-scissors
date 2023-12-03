758f0a2ce0533aace184e989794ff77de6d40565
jrbarona4 <jeromebarona1@gmail.com>
2023-12-03 19:37:49 +0800

    Add for loop to game function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let playerScore = 0;
 let computerScore = 0;
-let rounds = 0;
 
 function getComputerChoice() {
     const choice = ['rock', 'paper', 'scissors'];
@@ -16,33 +15,29 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection){
         return "It's a tie!";
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        rounds++;
         playerScore++;
         return "You win, rock beats scissors";
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        rounds++;
         playerScore++;
         return "You win, scissors beats paper";
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        rounds--;
         playerScore++;
         return "You win, paper beats rock";
     } else {
-        rounds--;
         computerScore++;
         return `You lose ${computerSelection} beats ${playerSelection}`;
     }
-
 }
 
 function game() {
-    for (i = 0; i <= rounds; i++) {
-        if (playerScore === 5){
-        return 'Player Wins';
-        } else if (computerScore === 5) {
-        return 'Computer Wins';
+    for (i = 0; i <= playerScore || computerScore; i++){
+        if (playerScore === 5 || computerScore === 5) {
+            console.log('Game Over');
+            break;
         } else {
-            playRound();
+            console.log(playRound());
+            console.log("Player: " + playerScore);
+            console.log("Computer: " + computerScore);
         }
     }
 }
